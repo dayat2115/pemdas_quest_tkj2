aef98707a45e91f203ed9c05a6401dedbe0f132f
dayat2115 <111479953+dayat2115@users.noreply.github.com>
2022-08-21 22:10:28 -0700

    Add files via upload

---
+++ python4-dayat.py
@@ -0,0 +1,15 @@
+nd=input("masukan nama depan:")
+nb=input("masukan nama belakang:")
+print("nama lengkap anda adalah",nd,nb)
+
+a=int(input("masukan nilai A:"))
+b=float(input("masukan nilai B:"))
+C=a+b
+
+print("A=", a)
+print("B", b)
+print(a,"+",b,"=",'c')
+
+x= int(input("masukan nilai mata uang: "))
+y= x *13752
+print(x,"dollar=",y,"rupiah")
